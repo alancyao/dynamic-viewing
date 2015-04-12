e80b34e9f1364b658df23918be68957c6b1a3fe7
Rohan <ronuchit@yahoo.com>
2015-04-11 17:37:44 -0700

    MULTISCALE WORKING

--- facial_recognition.py
+++ facial_recognition.py
@@ -1,6 +1,11 @@
 import sys
 import numpy as np
+from skimage import transform as tf
 import cv2
+
+WINDOW_AMT = 10
+DISP_SCALE = 0.7
+NUM_PYR = 2
 
 class FacialRecognition:
   def __init__(self):
@@ -13,10 +18,10 @@ class FacialRecognition:
     print "When face is visible, press Enter to continue."
     while self.cap.isOpened():
       ret, frame = self.cap.read()
-      frame = cv2.resize(frame, dsize=(0, 0), fx=0.7, fy=0.7)
       if not ret:
         print "Something is wrong..."
         return
+      frame = cv2.resize(frame, dsize=(0, 0), fx=DISP_SCALE, fy=DISP_SCALE)
       gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
       # TODO: our own haar cascade
       faces = self.face_cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5)
@@ -32,24 +37,46 @@ class FacialRecognition:
 
     x, y, w, h = faces[0]
     face_roi = frame[y:y+h, x:x+w]
+    face_pyramid = list(tf.pyramid_gaussian(face_roi, max_layer=NUM_PYR, downscale=2))
     while self.cap.isOpened():
       ret, frame = self.cap.read()
-      frame = cv2.resize(frame, dsize=(0, 0), fx=0.7, fy=0.7)
       if not ret:
         print "Something is wrong..."
         return
-      down_steps = (frame.shape[0] - h) / 10
-      right_steps = (frame.shape[1] - w) / 10
-      ssds = {}
-      for i in range(down_steps):
-        for j in range(right_steps):
-          cand_roi = frame[10*i:10*i+h, 10*j:10*j+w]
-          ssds[(i, j)] = ((face_roi - cand_roi) ** 2).sum()
-      best_i, best_j = min(ssds, key=lambda k: ssds[k])
-      cv2.rectangle(frame, (10*best_j, 10*best_i), (10*best_j + w, 10*best_i + h), color=(255, 0, 0), thickness=2)
-      # best_roi = frame[10*best_i:10*best_i+h, 10*best_j:10*best_j+w]
+      frame = cv2.resize(frame, dsize=(0, 0), fx=DISP_SCALE, fy=DISP_SCALE)
+      frame_pyramid = list(tf.pyramid_gaussian(frame, max_layer=NUM_PYR, downscale=2))
+
+      wa = int(WINDOW_AMT / (2 ** NUM_PYR))
+      region_indices = [0, (frame_pyramid[-1].shape[0] - face_pyramid[-1].shape[0]) / wa,
+                        0, (frame_pyramid[-1].shape[1] - face_pyramid[-1].shape[1]) / wa]
+      for pyr_index in reversed(range(NUM_PYR+1)):
+        res = self.compute_ssd(frame_pyramid[pyr_index],
+                               face_pyramid[pyr_index],
+                               2 ** pyr_index,
+                               region_indices)
+        if res is None:
+          break
+        best_i, best_j = res
+        region_indices = [best_i - 1, best_i + 1,
+                          best_j - 1, best_j + 1]
+      cv2.rectangle(frame, (WINDOW_AMT*best_j, WINDOW_AMT*best_i), (WINDOW_AMT*best_j + w, WINDOW_AMT*best_i + h), color=(255, 0, 0), thickness=2)
       cv2.imshow("frame", frame)
       cv2.waitKey(5)
+
+  def compute_ssd(self, frame, face, scaleAmt, region_indices):
+    wa = int(WINDOW_AMT / scaleAmt)
+    h = face.shape[0]
+    w = face.shape[1]
+    ssds = {}
+    for i in range(region_indices[0], region_indices[1]):
+      for j in range(region_indices[2], region_indices[3]):
+        cand_roi = frame[wa*i:wa*i+h, wa*j:wa*j+w]
+        if cand_roi.shape == face.shape:
+          ssds[(i, j)] = ((face - cand_roi) ** 2).sum()
+    if not ssds:
+      return None
+    best_i, best_j = min(ssds, key=lambda k: ssds[k])
+    return best_i, best_j
 
 def main():
   FacialRecognition().run()
