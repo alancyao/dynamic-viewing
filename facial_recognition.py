8d3aefb9602f1b96c2d86eab5e9624466f6d8223
alanyao <alanyao@berkeley.edu>
2015-04-11 21:40:03 -0700

    refactors

--- facial_recognition.py
+++ facial_recognition.py
@@ -2,12 +2,13 @@ import sys
 import numpy as np
 from skimage import transform as tf
 import cv2
+from threading import *
 
-DISP_SCALE = 0.7
 WINDOW_AMT = 10
+DISP_SCALE = 0.7
 NUM_PYR = 2
-# ROT_AMTS = np.linspace(-90, 90, num=20)
-ROT_AMTS = [0]
+AVERAGE_FACE_WIDTH = 250
+START_FACE_DIST = 310
 
 class FacialRecognition:
   def run(self):
@@ -35,46 +36,101 @@ class FacialRecognition:
     x, y, w, h = faces[0]
     face_roi = frame[y:y+h, x:x+w]
     face_pyramid = list(tf.pyramid_gaussian(face_roi, max_layer=NUM_PYR, downscale=2))
+
+
     while self.cap.isOpened():
       ret, frame = self.cap.read()
       if not ret:
         print "Something is wrong..."
         return
       frame = cv2.resize(frame, dsize=(0, 0), fx=DISP_SCALE, fy=DISP_SCALE)
+      frame_pyramid = list(tf.pyramid_gaussian(frame, max_layer=NUM_PYR, downscale=2))
 
-      frame_pyramids = [list(tf.pyramid_gaussian(tf.rotate(frame, angle=rot_amt),
-                                                 max_layer=NUM_PYR,
-                                                 downscale=2)) for rot_amt in ROT_AMTS]
-      ssds = {}
-      for i, frame_pyramid in enumerate(frame_pyramids):
-        ssds[ROT_AMTS[i]] = self.determine_best_shift(face_pyramid, frame_pyramid)
-      best_rot_amt = min(ssds, key=lambda k: ssds[k][2])
-      best_rot_amt *= np.pi / 180.0
-      best_i, best_j, best_ssd = ssds[best_rot_amt]
-      pixel_loc = np.array([WINDOW_AMT * best_i, WINDOW_AMT * best_j])
-      pixel_loc -= np.array(frame.shape[:2]) / 2
-      pixel_loc = np.array([[np.cos(best_rot_amt), -np.sin(best_rot_amt)], [np.sin(best_rot_amt), np.cos(best_rot_amt)]]).dot(pixel_loc)
-      pixel_loc += np.array(frame.shape[:2]) / 2
-      pixel_loc = map(int, pixel_loc)
-      cv2.rectangle(frame, (pixel_loc[1], pixel_loc[0]), (pixel_loc[1] + w, pixel_loc[0] + h), color=(255, 0, 0), thickness=2)
+      best_i, best_j = self.determine_best_shift(face_pyramid, frame_pyramid)
+      cv2.rectangle(frame, (WINDOW_AMT*best_j, WINDOW_AMT*best_i), (WINDOW_AMT*best_j + w, WINDOW_AMT*best_i + h), color=(255, 0, 0), thickness=2)
       cv2.imshow("frame", frame)
       cv2.waitKey(5)
+
+  def calibrate(self):
+    self.cap = cv2.VideoCapture(0)
+    print "PLace face 1 ft from camera. When face is visible, press Enter to continue."
+    while self.cap.isOpened():
+      ret, frame = self.cap.read()
+      if not ret:
+        print "Something is wrong..."
+        return
+      frame = cv2.resize(frame, dsize=(0, 0), fx=DISP_SCALE, fy=DISP_SCALE)
+      gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+      face_cascade = cv2.CascadeClassifier("haarcascades/haarcascade_frontalface_default.xml")
+      faces = face_cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5)
+      for x, y, w, h in faces:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), color=(255, 0, 0), thickness=2)
+      cv2.imshow("calibration", frame)
+      if cv2.waitKey(1) & 0xFF == 10:
+        cv2.destroyWindow("calibration")
+        if len(faces) > 0:
+          break
+        else:
+          print "No face detected."
+
+    x, y, w, h = faces[0]
+    self.face_roi = frame[y:y+h, x:x+w]
+    self.face_pyramid = list(tf.pyramid_gaussian(self.face_roi, max_layer=NUM_PYR, downscale=2))
+    # w = f*Y/Z  -->  f = wZ/Y
+    self.camera_f = w * START_FACE_DIST/AVERAGE_FACE_WIDTH
+    self.start_center = np.array((x + w/2.0, y+h/2.0))
+    self.w = w; self.h = h
+    cv2.destroyWindow("calibration")
+    cv2.waitKey(1)
+    self.wtf()
+
+  def wtf(self):
+    cv2.destroyWindow("calibration")
+    cv2.waitKey(1)
+    cv2.destroyWindow("calibration")
+    cv2.waitKey(1)
+    cv2.destroyWindow("calibration")
+    cv2.destroyWindow("calibration")
+    cv2.waitKey(1)
+    cv2.destroyWindow("calibration")
+    cv2.waitKey(1)
+    cv2.destroyWindow("calibration")
+    cv2.waitKey(1)
+    cv2.destroyWindow("calibration")
+    cv2.waitKey(1)
+
+  def get_face(self):
+    ret, frame = self.cap.read()
+    if not ret:
+      print "Something is wrong..."
+      return
+    frame = cv2.resize(frame, dsize=(0, 0), fx=DISP_SCALE, fy=DISP_SCALE)
+    frame_pyramid = list(tf.pyramid_gaussian(frame, max_layer=NUM_PYR, downscale=2))
+
+    best_i, best_j = self.determine_best_shift(self.face_pyramid, frame_pyramid)
+    return (np.array((WINDOW_AMT*best_j, WINDOW_AMT*best_i)) + np.array((WINDOW_AMT*best_j + self.w, WINDOW_AMT*best_i + self.h))) / 2.0
+
+  def get_rotation(self):
+    center = self.get_face()
+    disp = center - self.start_center
+    rot = np.arctan(disp/START_FACE_DIST) * (180 / np.pi)  #change to actual face dist
+    return rot
 
   def determine_best_shift(self, face_pyramid, frame_pyramid):
     wa = int(WINDOW_AMT / (2 ** NUM_PYR))
     region_indices = [0, (frame_pyramid[-1].shape[0] - face_pyramid[-1].shape[0]) / wa,
-                      0, (frame_pyramid[-1].shape[1] - face_pyramid[-1].shape[1]) / wa]
+        0, (frame_pyramid[-1].shape[1] - face_pyramid[-1].shape[1]) / wa]
     for pyr_index in reversed(range(NUM_PYR+1)):
       res = self.compute_ssd(frame_pyramid[pyr_index],
-                             face_pyramid[pyr_index],
-                             2 ** pyr_index,
-                             region_indices)
+          face_pyramid[pyr_index],
+          2 ** pyr_index,
+          region_indices)
       if res is None:
         break
-      best_i, best_j, best_ssd = res
+      best_i, best_j = res
       region_indices = [best_i - 1, best_i + 1,
-                        best_j - 1, best_j + 1]
-    return best_i, best_j, best_ssd
+          best_j - 1, best_j + 1]
+      return best_i, best_j
 
   def compute_ssd(self, frame, face, scaleAmt, region_indices):
     wa = int(WINDOW_AMT / scaleAmt)
@@ -89,10 +145,10 @@ class FacialRecognition:
     if not ssds:
       return None
     best_i, best_j = min(ssds, key=lambda k: ssds[k])
-    return best_i, best_j, ssds[(best_i, best_j)]
+    return best_i, best_j
 
 def main():
   FacialRecognition().run()
-        
+
 if __name__ == "__main__":
   main()
