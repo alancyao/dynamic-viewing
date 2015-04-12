d9d3b37c5a57a5a19aac9c1765bf1c547c35b728
alanyao <alanyao@berkeley.edu>
2015-04-12 00:55:59 -0700

    interpolating bounding boxes

--- facial_recognition.py
+++ facial_recognition.py
@@ -2,7 +2,7 @@ import sys
 import numpy as np
 from skimage import transform as tf
 import cv2
-from threading import *
+from threading import Thread
 
 NUM_PYR = 2
 WINDOW_AMT = 8
@@ -10,6 +10,7 @@ assert (float(WINDOW_AMT) / (2 ** NUM_PYR)).is_integer(), "WINDOW_AMT must remai
 DISP_SCALE = 0.7
 AVERAGE_FACE_WIDTH = 250
 START_FACE_DIST = 310
+RESCALING_FACTORS = [0.7, 1, 1.3]
 
 
 class WebcamImageGetter:
@@ -23,7 +24,8 @@ class WebcamImageGetter:
 
   def updateFrame(self):
     while self.keep_going:
-      ret, self.currentFrame = self.capture.read()
+      ret, frame = self.capture.read()
+      self.currentFrame= cv2.resize(frame, dsize=(0, 0), fx=DISP_SCALE, fy=DISP_SCALE)
 
   def getFrame(self):
     return self.currentFrame
@@ -39,12 +41,13 @@ class FacialRecognition:
     print "Tracking face..."
 
     while True:
-      best_i, best_j, frame = self.get_face()
+      best_i, best_j, frame, interp_shape = self.get_face()
       # Display bounding box
-      cv2.rectangle(frame, (WINDOW_AMT*best_j, WINDOW_AMT*best_i), (WINDOW_AMT*best_j + self.w, WINDOW_AMT*best_i + self.h), color=(255, 0, 0), thickness=2)
+      cv2.rectangle(frame, (WINDOW_AMT*best_j, WINDOW_AMT*best_i), (WINDOW_AMT*best_j + interp_shape[1], WINDOW_AMT*best_i + interp_shape[0]), color=(255, 0, 0), thickness=2)
       cv2.imshow("frame", frame)
       if cv2.waitKey(1) & 0xFF == 10:
-        self.end()
+        cv2.destroyWindow("frame")
+        self.ig.keep_going = False
         break
 
   def calibrate(self):
@@ -55,7 +58,6 @@ class FacialRecognition:
       frame = self.ig.getFrame()
       if frame is None:
         continue
-      frame = cv2.resize(frame, dsize=(0, 0), fx=DISP_SCALE, fy=DISP_SCALE)
       gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
       face_cascade = cv2.CascadeClassifier("haarcascades/haarcascade_frontalface_default.xml")
       faces = face_cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5)
@@ -70,8 +72,12 @@ class FacialRecognition:
           print "No face detected."
 
     x, y, w, h = faces[0]
-    self.face_roi = frame[y:y+h, x:x+w]
-    self.face_pyramid = list(tf.pyramid_gaussian(self.face_roi, max_layer=NUM_PYR, downscale=2))
+    num_pix = float(w*h)
+    face_roi = frame[y:y+h, x:x+w]
+    scaled_faces = [tf.rescale(face_roi, scale=sc) for sc in RESCALING_FACTORS]
+    self.face_pyramids = [list(tf.pyramid_gaussian(face, max_layer=NUM_PYR, downscale=2))
+                          for face in scaled_faces]
+    self.scaled_weights = [num_pix / (sf.shape[0]*sf.shape[1]) for sf in scaled_faces]
     # w = f*Y/Z  -->  f = wZ/Y
     self.camera_f = w * START_FACE_DIST/AVERAGE_FACE_WIDTH
     self.start_center = np.array((x + w/2.0, y+h/2.0))
@@ -85,17 +91,28 @@ class FacialRecognition:
     frame = self.ig.getFrame()
     frame_pyramid = list(tf.pyramid_gaussian(frame, max_layer=NUM_PYR, downscale=2))
 
-    best_i, best_j = self.determine_best_shift(self.face_pyramid, frame_pyramid)
-    cv2.rectangle(frame, (WINDOW_AMT*best_j, WINDOW_AMT*best_i), (WINDOW_AMT*best_j + self.w, WINDOW_AMT*best_i + self.h), color=(255, 0, 0), thickness=2)
-    cv2.imshow("frame", frame)
-    cv2.waitKey(1)
-    return best_i, best_j, frame
+    ssds = {}
+    for i, face_pyramid in enumerate(self.face_pyramids):
+      best_i, best_j, best_ssd = self.determine_best_shift(face_pyramid, frame_pyramid)
+      ssds[i] = (best_ssd * self.scaled_weights[i], best_i, best_j, np.array(face_pyramid[0].shape))
+    sorted_ssds = sorted(ssds.items(), key=lambda x : x[1][0])
+    best_ssd, best_i, best_j, best_shape = sorted_ssds[0][1]
+    sbest_ssd, best_i, sbest_j, sbest_shape = sorted_ssds[1][1]
+    interp_shape = map(int, best_ssd / (best_ssd + sbest_ssd) * sbest_shape + \
+                            sbest_ssd / (best_ssd + sbest_ssd) * best_shape)
+    print interp_shape
+    return best_i, best_j, frame, interp_shape
 
   def get_rotation(self):
-    best_i, best_j, frame = self.get_face()
+    best_i, best_j, frame, interp_shape = self.get_face()
     center = (np.array((WINDOW_AMT*best_j, WINDOW_AMT*best_i)) + np.array((WINDOW_AMT*best_j + self.w, WINDOW_AMT*best_i + self.h))) / 2.0
     disp = center - self.start_center
     rot = np.arctan(disp/START_FACE_DIST) * (180 / np.pi) # change to actual face dist
+
+    # Display image (for fun)
+    cv2.rectangle(frame, (WINDOW_AMT*best_j, WINDOW_AMT*best_i), (WINDOW_AMT*best_j + interp_shape[1], WINDOW_AMT*best_i + interp_shape[0]), color=(255, 0, 0), thickness=2)
+    cv2.imshow("frame", frame)
+    cv2.waitKey(1)
     return np.array(rot)
 
   def determine_best_shift(self, face_pyramid, frame_pyramid):
@@ -109,10 +126,12 @@ class FacialRecognition:
                              region_indices)
       if res is None:
         break
-      best_i, best_j = res
+      best_i, best_j, tmp_ssd = res
+      if pyr_index == NUM_PYR:
+          best_ssd = tmp_ssd
       region_indices = [best_i - 1, best_i + 1,
                         best_j - 1, best_j + 1]
-    return best_i, best_j
+    return best_i, best_j, best_ssd
 
   def compute_ssd(self, frame, face, scaleAmt, region_indices):
     wa = int(WINDOW_AMT / scaleAmt)
@@ -127,7 +146,7 @@ class FacialRecognition:
     if not ssds:
       return None
     best_i, best_j = min(ssds, key=lambda k: ssds[k])
-    return best_i, best_j
+    return best_i, best_j, ssds[(best_i, best_j)]
 
 def main():
   FacialRecognition().run()
